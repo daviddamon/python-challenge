7eadb1d0cc1539e7b93c8e34c35f2ea947b212a3
David Damon <dave@daviddamon.com>
2020-07-07 11:04:39 -0400

    simplified code with no dictionary

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -18,15 +18,16 @@
 # modules
 import os
 import csv
-#import collections
 
 # initialize variables
 total_votes = 0
+kahn_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 # create lists to store data
-names = []
-votes = []
-voting_data = ['1', '2']
+winner = []
 
 # set path for data file
 csvpath = os.path.join('Resources', 'test_data.csv')
@@ -36,11 +37,9 @@ with open(csvpath, 'r') as csvfile:
 
      csvreader = csv.reader(csvfile, delimiter=',')
 
-     print (csvreader)
-
-     # read the header row first
+     # read csv header and skip to first line of data
      csv_header = next(csvreader)
-     print(f"CSV Header: {csv_header}")  
+     print(f"Header: {csv_header}")
 
      # begin loop to read each row of data file after header
      for row in csvreader:
@@ -48,102 +47,76 @@ with open(csvpath, 'r') as csvfile:
 
           # increment total vote count
           total_votes += 1
+          print(total_votes) 
 
-          name = row[1]
-          vote = row[2]
+          if row[2] == "Kahn"
+               kahn_votes += 1
 
-          # loop to add names and votes to running total
-          if name in names:
-               name_index = names.index(name)
-               votes[name_index] = votes[name_index] + 1
+          elif row[2] == "Correy"
+               correy_votes += 1
 
-          else:
-               names.append(name)
-               votes.append(vote)
+          elif row[2] == "Li"
+               li_votes += 1
 
-     # declare more variables for next loop
-     percent = []
-     highest = vote[0]
-     highest_index = 0
+          elif row[2] == "O'Tooley"
+               otooley_votes += 1
 
-     # calculate percentage and winner
-     for count in range(len(name)):
-          vote_percent = vote[count]/total_votes*100
-          percent.append(vote_percent)
+          else
 
-     if vote[count] > highest:
-          highest = vote[count]
-          print(highest)
-          highest_index = count
+          print("Kahn:  " + kahn_votes)
+          print("Correy:  " + correy_votes)
+          print("Li:  " + li_votes)
+          print("O'Tooley:  " + otooley_votes)
 
-     # declare winner 
-     win_name = names[highest_index]
-   
-     # calculate percentage of votes
-     percent = [round(i,3) for i in percent]
-    
-     ##################################################################
+     # check for winner
+     if kahn_votes > (correy_votes and li_votes and otooley_votes)
+          winner = "Kahn"
 
-     # find all candidate names and create new list
+          elif correy_votes > (li_votes and otooley_votes and kahn_votes)
+          winner = "Correy"
 
-     #unique_name.append(set(candidate))
+          elif li_votes > (otooley_votes and kahn_votes and correy_votes)
+          winner = "Li"
 
-     # count votes per candidate
-     #candidate_votes = unique_candidate_name.count
+          elif otooley_votes > (kahn_votes and correy_votes and li_votes)
+          winner = "O'Tooley"
+     
+     else
 
-     #print counts #prints list with counts for eack
-     #x = list(counts.elements())
-     #print x
-
-     #y = counts.most_common(2)  # counts top two winners
-     #print y[1]  # prints top winner only
-
-     #winner = unique_candidate_name[candidate_votes.index(max(candidate_votes))]
+     print(winner)
 
 
-     #######################################################
-
-#-------------------------
-#Khan: 63.000% (2218231)
-#Correy: 20.000% (704200)
-#Li: 14.000% (492940)
-#O'Tooley: 3.000% (105630)
-#-------------------------
-#Winner: Khan
-#-------------------------
 
 # print to terminal
-
 print('Election Results') 
-print('-------------------------')
+print('----------------------------------------')
 print(f'Total Votes: {total_votes}')
-print('-------------------------')
+print('----------------------------------------')
+print('Kahn: ' + round(kahn_votes / total_votes * 100,3) + '%  (' + kahn_votes + ')'
+print('Correy: ' + round(correy_votes / total_votes * 100,3) + '%  (' + correy_votes + ')'
+print('Li: ' + round(li_votes / total_votes * 100,3) + '%  (' + li_votes + ')'
+print('O\'Tooley: ' + round(otooley_votes / total_votes * 100,3) + '%  (' + otooley_votes + ')'
+print('----------------------------------------')
+print(f'Winner: {winner}')
+print('----------------------------------------')
 
-for count in range(len(name)):
-     print(f'{name[count]}: {percent[count]}%  ({votes[count]})')
-
-print('-------------------------')
-print(f'Winner: {win_name}')
-print('-------------------------')
-
-
-# write new text document
+# write new text document in different directory
 # set destination file path
-txtpath = os.path.join('..', 'Analysis')
+#filepath = os.path.join('..', 'Analysis')
 
 # create a text file in Analysis folder  
-filename = ('txtpath', 'vote_summary.txt')
+filename = 'Analysis/Pypoll_results.txt'
 with open(filename, 'w') as output:
 
-     output.write('Election Results')
-     output.write('-------------------------')
+     output.write('Election Results') 
+     output.write('----------------------------------------')
      output.write(f'Total Votes: {total_votes}')
-     output.write('-------------------------')
-
-     for count in range(len(name)):
-          output.write(f'{name[count]}: {percent[count]}%  ({votes[count]})')
-
-     output.write('-------------------------')
-     output.write(f'Winner: {win_name}')
-     output.write('-------------------------')
- 
+     output.write('----------------------------------------')
+     output.write('Kahn: ' + round(kahn_votes / total_votes * 100,3) + '%  (' + kahn_votes + ')'
+     output.write('Correy: ' + round(correy_votes / total_votes * 100,3) + '%  (' + correy_votes + ')'
+     output.write('Li: ' + round(li_votes / total_votes * 100,3) + '%  (' + li_votes + ')'
+     output.write('O\'Tooley: ' + round(otooley_votes / total_votes * 100,3) + '%  (' + otooley_votes + ')'
+     output.write('----------------------------------------')
+     output.write(f'Winner: {winner}')
+     output.write('----------------------------------------')
+    
